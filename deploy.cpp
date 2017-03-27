5f3774581d1eae3e6744d36fa805930aecb1d5e7
AXIOMZK <axiomzk@outlook.com>
2017-03-27 18:03:15 +0800

    1.服务器编号及排序 2.退火产生新解策略
    next：
    1.遗产算法产生新解
    2.退火模型
    3.遗传算子框架搭建

--- deploy.cpp
+++ deploy.cpp
@@ -1,36 +1,85 @@
 #include "deploy.h"
-#include <iostream>
-#include <sstream>
-#include <vector>
 
 using namespace std;
 //C++整数规划+模拟退火方案
-struct NetsInfo
+
+int isExit = 1;//定时器标志
+//通过调用alarm来设置计时器，然后继续做别的事情。当计时器计时到0时，信号发送，处理函数被调用。
+
+void timer(int sig)
 {
-    int total_bandwidth = 0;
-    int remaining_bandwidth = 0;
-    int network_hire = 0;
-};
-struct ResumeInfo
+    isExit = 0;
+}
+
+//服务器总成本
+int getServerCost(vector<SeverNoAndAroundBandwidth> &v)
 {
-    int node_NO;
-    int need_bandwidth;
+    return SeverCost * (int) v.size();
+}
+
+//得到新服务器编号(变异)
+
+set<SeverNoAndAroundBandwidth> getNewServe(set<SeverNoAndAroundBandwidth> &oldServe)
+{
+    int size= (int) oldServe.size();
+    auto newServe = oldServe;
+    srand((unsigned int) time(NULL));
+    int flag = 1 + rand() % 5;
+    if (flag == 1)
+    {
+        //随机删除一个服务器
+        int pos = (int) (rand() % newServe.size());
+        auto it = newServe.begin();
+        while (pos--) it++;
+        newServe.erase(it);
+
+    } else if (flag == 2)
+    {
+        //优先删除所能提供带宽最小的服务器
+        newServe.erase(newServe.begin());
+
+    } else if (flag == 3)
+    {
+        //随机添加一个服务器
+        int pos = (int) (rand() % newServe.size());
+
+        auto it = newServe.begin();
+
+    } else if (flag == 4)
+    {
+
+
+    } else
+    {
+
+    }
+}
+
+struct Bandwidth_From_Small_To_Big
+{
+    bool operator()(const SeverNoAndAroundBandwidth &left, const SeverNoAndAroundBandwidth &right) const
+    {
+        return (left.ServeAroundBandwidth < right.ServeAroundBandwidth);
+    }
 };
 
 void deploy_server(char *topo[MAX_EDGE_NUM], int line_num, char *filename)
 {
-    unsigned long links, consumer_nodes, network_nodes;
+    //执行定时器函数
+    signal(SIGALRM, timer);
+    alarm(80); //定时80s
+
     stringstream read(topo[0]);
     read >> network_nodes >> links >> consumer_nodes;
-    vector<vector<NetsInfo>> Nets(network_nodes, vector<NetsInfo>(network_nodes));
-    vector<ResumeInfo> Consumers(consumer_nodes);
-    int cost = atoi(topo[2]);
+    maxServer = consumer_nodes;
+    SeverCost = atoi(topo[2]);
 
     for (unsigned long i = 4; i < 4 + links; ++i)
     {
-        double start_node, end_node;int total_bandwidth, network_hire;
+        double start_node, end_node;
+        int total_bandwidth, network_hire;
         read.str("");
-        read<<topo[i];
+        read << topo[i];
         read >> start_node >> end_node >> total_bandwidth >> network_hire;
         Nets[start_node][end_node].total_bandwidth = total_bandwidth;
         Nets[start_node][end_node].remaining_bandwidth = total_bandwidth;
@@ -40,25 +89,49 @@ void deploy_server(char *topo[MAX_EDGE_NUM], int line_num, char *filename)
     {
         int num, node_NO, need_bandwidth;
         read.str("");
-        read<<topo[j];
+        read << topo[j];
         read >> num >> node_NO >> need_bandwidth;
         Consumers[num].need_bandwidth = need_bandwidth;
         Consumers[num].node_NO = node_NO;
     }
 
+    //得到服务器节点可能的最大输出带宽
+    int AroundBandwidth = 0;
+    for (int i = 0; i < network_nodes; ++i)
+    {
+        for (int j = 0; j < network_nodes; ++j)
+        {
+            AroundBandwidth += Nets[i][j].total_bandwidth;
+        }
+        ServeAroundBandwidth.push_back(AroundBandwidth);
+    }
+
+    //服务器初始编号，即直连方案编号，外加一个参考可能提供带宽量,按带宽从小到大排序
+    set<SeverNoAndAroundBandwidth,Bandwidth_From_Small_To_Big> SeverNo;
+    for (int l = 0; l < Consumers.size(); ++l)
+    {
+        SeverNoAndAroundBandwidth pair;
+        pair.ServerNo = l;
+        pair.ServeAroundBandwidth = ServeAroundBandwidth[l];
+        SeverNo.insert(pair);
+    }
+
+
     read.str("");
-    stringstream &results=read;
-    results << consumer_nodes<<"\n";
+    stringstream &results = read;
+    results << consumer_nodes << "\n";
     for (unsigned long k = 0; k < consumer_nodes; ++k)
     {
-        results<<"\n"<<Consumers[k].node_NO<<" "<<k<<" "<<Consumers[k].need_bandwidth;
+        results << "\n" << Consumers[k].node_NO << " " << k << " " << Consumers[k].need_bandwidth;
     }
-    const string& strtemp = results.str();
-    char* topo_file = (char *) strtemp.c_str();
+    const string &strtemp = results.str();
+    char *topo_file = (char *) strtemp.c_str();
+
+
 
 //    char* topo_file=(char *)results.str().c_str();//这是错误的
 //streamstring在调用str()时，会返回临时的string对象。而因为是临时的对象，所以它在整个表达式结束后将会被析构。
- //   如果需要进一步操作string对象，先把其值赋给一个string变量后再操作。
+    //   如果需要进一步操作string对象，先把其值赋给一个string变量后再操作。
     //alarm定时器，捕捉SIGALRM信号
     //捕捉函数用 signal(SIGALRM, funcPtr);
     // 需要输出的内容
@@ -68,3 +141,5 @@ void deploy_server(char *topo[MAX_EDGE_NUM], int line_num, char *filename)
     write_result(topo_file, filename);
 
 }
+
+
--- deploy.h
+++ deploy.h
@@ -2,9 +2,46 @@
 #define __ROUTE_H__
 
 #include "lib_io.h"
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <set>
+#include <unistd.h>
 
-void deploy_server(char * graph[MAX_EDGE_NUM], int edge_num, char * filename);
+using namespace std;
+//服务器部署编号(连接的节点号)，评估带宽结构体
+struct SeverNoAndAroundBandwidth
+{
+    int ServerNo;
+    int ServeAroundBandwidth;
+};
+//链路信息(双向)
+struct LinkInfo
+{
+    int total_bandwidth = 0;
+    int remaining_bandwidth = 0;
+    int network_hire = 0;
+};
+//消费节点信息结构体
+struct ResumeInfo
+{
+    int node_NO;//消费节点连接的网络节点编号
+    int need_bandwidth;//需求带宽
+};
+int SeverCost;//单台服务器成本
+unsigned long maxServer;//最大服务器数，即消费节点数
+unsigned long links, consumer_nodes, network_nodes;//链路数，消费节点数，网络节点数
+vector<int> ServeAroundBandwidth(network_nodes);//序号为服务器所连的节点号，值为评估带宽
+vector<vector<LinkInfo>> Nets(network_nodes, vector<LinkInfo>(network_nodes));
+vector<ResumeInfo> Consumers(consumer_nodes);//vector序号为消费节点编号
 
-	
+
+void deploy_server(char *graph[MAX_EDGE_NUM], int edge_num, char *filename);
+
+void timer(int sig);
+
+int getServerCost(vector<SeverNoAndAroundBandwidth> &v);//得到部署服务器的成本
+set<SeverNoAndAroundBandwidth> getNewServe(set<SeverNoAndAroundBandwidth> &oldServe);
+//模拟退火产生新的服务器编号组合
 
 #endif
