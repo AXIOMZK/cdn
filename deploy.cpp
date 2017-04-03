46176f71583ee820971fa72f6d0f120da9d6077f
AXIOMZK <axiomzk@outlook.com>
2017-04-04 02:42:20 +0800

    最好版本

--- deploy.cpp
+++ deploy.cpp
@@ -122,14 +122,14 @@ void deploy_server(char *topo[MAX_EDGE_NUM], int line_num, char *filename)
         {
             //执行定时器函数
             signal(SIGALRM, timer);
-//            alarm(88); //定时80s
+            alarm(88); //定时80s
             T = 1000;     //初始温度
             EPS = 1e-9;     //终止温度
             DELTA = 0.98;     //温度衰减率
-            LIMIT = 250;     //概率选择上限
+            LIMIT = 10;     //概率选择上限
             OLOOP = 2000;      //外循环次数
             ILOOP = 1000;      //内循环次数
-            p0 = 1.2;
+            p0 = 1.0;
         } else
         {
             //执行定时器函数
